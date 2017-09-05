51414ff29e35fe81f151f6b62448a7d69cdd84fa
lourencofelipe <felipe.pucsc@gmail.com>
2017-09-05 20:44:17 -0300

    Update Exer0AvancBinary.java

--- Avancado/AvancadoExercicio00/Exer0AvancBinary.java
+++ Avancado/AvancadoExercicio00/Exer0AvancBinary.java
@@ -2,13 +2,49 @@ package AvancadoExercicio00;
 
 /* Transformar inteiro em binario */
 //https://stackoverflow.com/questions/2406432/converting-an-int-to-a-binary-string-representation-in-java
-public class Exer0AvancBinary {
-	public static String convert(int n) {
-		  return Integer.toBinaryString(n);
-		}
+package conversorBinario;
 
-		public static void main(String[] args) {
-		  System.out.println(convert(20)); 
-		  System.out.println(convert(54)); 
-		}
+public class Calc {
+
+	private String integerString;
+
+	public String getIntegerString() {
+		return integerString;
+	}
+
+	public void setIntegerString(String integerString) {
+		this.integerString = integerString;
+	}
+
+	public String calculo() {
+		System.out.println(Integer.toBinaryString(Integer
+				.parseInt(integerString)));
+		System.out.print("Número em decimal digitado: ");
+		return integerString;
+
+	}
+}
+
+package conversorBinario;
+
+import java.util.Scanner;
+
+public class ConversorCalc {
+
+	public static void main(String[] args) {
+		String IntegerString;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Informe um valor inteiro: ");
+		IntegerString = input.nextLine();
+
+		Calc novoObjt = new Calc();
+
+		novoObjt.setIntegerString(IntegerString);
+
+		System.out.println("O valor convertido para binário é: "
+				+ novoObjt.calculo());
+
+	}
 }
